dc0a38ecb2a54df76d4e9365d4f9da3bf0ce56ad
debasishsahoo <debasish.sahoo96@gmail.com>
2022-03-31 12:25:33 +0530

    Stack

--- 4.Collections/Program.cs
+++ 4.Collections/Program.cs
@@ -11,70 +11,202 @@ namespace _4.Collections
     {
         static void Main(string[] args)
         {
-            ArrayList A1 = new ArrayList();
+            //ArrayList A1 = new ArrayList();
 
-            Console.WriteLine("----------------------Data Capacity------------------------");
-            Console.WriteLine("Initial Capacity:"+A1.Capacity);
+            //Console.WriteLine("----------------------Data Capacity------------------------");
+            //Console.WriteLine("Initial Capacity:"+A1.Capacity);
 
-            Console.WriteLine("----------------------Data Add------------------------");
-            A1.Add(10);
-            Console.WriteLine("After First Element Capacity:" + A1.Capacity);
+            //Console.WriteLine("----------------------Data Add------------------------");
+            //A1.Add(10);
+            //Console.WriteLine("After First Element Capacity:" + A1.Capacity);
 
-            A1.Add("KUNDAN");
-            Console.WriteLine("After Second Element Capacity:" + A1.Capacity);
+            //A1.Add("KUNDAN");
+            //Console.WriteLine("After Second Element Capacity:" + A1.Capacity);
 
-            A1.Add(true);
-            Console.WriteLine("After Third Element Capacity:" + A1.Capacity);
+            //A1.Add(true);
+            //Console.WriteLine("After Third Element Capacity:" + A1.Capacity);
 
-            A1.Add(3.14f);
-            Console.WriteLine("After Fourth Element Capacity:" + A1.Capacity);
-            A1.Add(3.14f);
-            Console.WriteLine("After Fourth Element Capacity:" + A1.Capacity);
-            A1.Add(3.14f);
-            Console.WriteLine("After Fourth Element Capacity:" + A1.Capacity);
+            //A1.Add(3.14f);
+            //Console.WriteLine("After Fourth Element Capacity:" + A1.Capacity);
+            //A1.Add(3.14f);
+            //Console.WriteLine("After Fourth Element Capacity:" + A1.Capacity);
+            //A1.Add(3.14f);
+            //Console.WriteLine("After Fourth Element Capacity:" + A1.Capacity);
 
-            A1.Add(null);
-            Console.WriteLine("After Six Element Capacity:" + A1.Capacity);
+            //A1.Add(null);
+            //Console.WriteLine("After Six Element Capacity:" + A1.Capacity);
 
-            A1.Add('M');
-            Console.WriteLine("After Fifth Element Capacity:" + A1.Capacity);
+            //A1.Add('M');
+            //Console.WriteLine("After Fifth Element Capacity:" + A1.Capacity);
 
             
 
-            Console.WriteLine("----------------------Data View------------------------");
+            //Console.WriteLine("----------------------Data View------------------------");
 
-            for (int i=0;i<A1.Count;i++)
+            //for (int i=0;i<A1.Count;i++)
+            //{
+            //    Console.WriteLine(A1[i]+" ");
+            //}
+            //Console.WriteLine("---------------------Data Delete-------------------------");
+
+            //A1.Remove(true);
+            //A1.Remove(null);
+            //A1.RemoveAt(2);
+
+            //foreach(object obj in A1)
+            //{
+            //    Console.WriteLine(obj+" ");
+            //}
+            //Console.WriteLine("---------------------Data Insert-------------------------");
+
+            //A1.Insert(1,"Asif");
+
+            //foreach (object obj in A1)
+            //{
+            //    Console.WriteLine(obj + " ");
+            //}
+
+            //Console.WriteLine("---------------------Array List Colne -------------------------");
+            //ArrayList Col1=new ArrayList(A1);
+
+            //Console.WriteLine("Initial Capacity Col1:"+Col1.Capacity);
+
+            //foreach (object obj in Col1)
+            //{
+            //    Console.WriteLine(obj + " ");
+            //}
+
+            //ArrayList Emp=new ArrayList();
+            //Emp.Add(1002);
+            //Emp.Add("Kundan");
+            //Emp.Add("SEARCHING");
+
+            //Emp.Add(1003);
+            //Emp.Add("Abir");
+            //Emp.Add("POKEING");
+
+            //Emp.Add(1004);
+            //Emp.Add("MIR");
+            //Emp.Add("143");
+
+            //Emp.Add(1005);
+            //Emp.Add("Debasish");
+            //Emp.Add("TRAP");
+
+            //Console.WriteLine("MIR:"+Emp[8]);
+
+
+
+            Hashtable hashtable = new Hashtable();
+
+            //hashtable.Add("ID",1002);
+            //hashtable.Add(hashtable["ID"], "Kundan");
+            //hashtable.Add(hashtable[1002], "SEARCHING");
+
+
+
+            //hashtable.Add("id", 1004);
+            //hashtable.Add("name", "MIR");
+            //hashtable.Add("job", "143");
+
+            //hashtable.Add("id", 1005);
+            //hashtable.Add("name", "Debasish");
+            //hashtable.Add("job", "TRAP");
+
+            //hashtable.Add("id", 1003);
+            //hashtable.Add("name", "Abir");
+            //hashtable.Add("job", "POKEING");
+
+            //Console.WriteLine("ID:" + hashtable["id"]);
+
+            //foreach (object key in hashtable.Keys)
+            //{
+            //    Console.WriteLine(key +":"+ hashtable[key]);
+            //}
+
+            //Console.WriteLine("is Email id Key Exist:"+ hashtable.ContainsKey("Emailid"));
+            //Console.WriteLine("is Email id Key Exist:" +hashtable.ContainsKey("name"));
+
+
+            //Console.WriteLine("is Kundan value Exist:" + hashtable.ContainsValue("kundan"));
+            //Console.WriteLine("is Abir Value  Exist:" + hashtable.ContainsValue("Abir"));
+
+
+            Stack S= new Stack();
+
+            S.Push(10);
+            S.Push("Kunden");
+            S.Push(3.14f);
+            S.Push(true);
+            S.Push(67.1f);
+            S.Push('A');
+
+            foreach(object obj in S)
             {
-                Console.WriteLine(A1[i]+" ");
+                Console.WriteLine(obj + " ");
             }
-            Console.WriteLine("---------------------Data Delete-------------------------");
+            Console.WriteLine("-------------------------");
 
-            A1.Remove(true);
-            A1.Remove(null);
-            A1.RemoveAt(2);
+            Console.WriteLine(S.Pop());
 
-            foreach(object obj in A1)
+            Console.WriteLine("--------------------------");
+
+            foreach(object obj in S)
             {
                 Console.WriteLine(obj+" ");
             }
-            Console.WriteLine("---------------------Data Insert-------------------------");
+            Console.WriteLine("-------------------------");
 
-            A1.Insert(1,"Asif");
+            Console.WriteLine(S.Peek());
 
-            foreach (object obj in A1)
-            {
-                Console.WriteLine(obj + " ");
-            }
+            Console.WriteLine("--------------------------");
 
-            Console.WriteLine("---------------------Array List Colne -------------------------");
-            ArrayList Col1=new ArrayList(A1);
+Console.WriteLine($"No of Element in the Collections:{S.Count}");
+Console.WriteLine($"Value present in Collections:{S.Contains("Kunden")}");
 
-            Console.WriteLine("Initial Capacity Col1:"+Col1.Capacity);
+            S.Clear();
 
-            foreach (object obj in Col1)
-            {
-                Console.WriteLine(obj + " ");
-            }
+            Console.WriteLine($"No of Element in the Collections after Clear Method :{S.Count}");
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             Console.ReadKey();
 
         }
